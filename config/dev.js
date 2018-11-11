65c5c4c8206dd86313243db42cb37b9525e5175a
oorochimaru <ryambudha@gmail.com>
2018-11-11 15:49:48 +0530

    nodestarter

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+    mongoURI: 'mongodb://ryamseyryam:Asdfg123@ds255463.mlab.com:55463/advancenode',
+
+  cookieKey: '123123123'
+};
---
+++ routes/blogRoutes.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+
+const Blog = mongoose.model('Blog');
+
+module.exports = app => {
+  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+    const blog = await Blog.findOne({
+      _user: req.user.id,
+      _id: req.params.id
+    });
+
+    res.send(blog);
+  });
+
+  app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis =  require('redis');
+    const rediURI = 'redis://127.0.0.1:6379'
+    const client = redis.createClient(rediURI);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+    const redisCheck = await client.get(req.user.id);
+    if (redisCheck) {
+      console.log('Serving from REDIS');
+      return res.send(JSON.parse(redisCheck));
+    }
+    
+    console.log('Serving from MONODB');
+    const blogs = await Blog.find({ _user: req.user.id });
+    res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
+  });
+
+  app.post('/api/blogs', requireLogin, async (req, res) => {
+    const { title, content } = req.body;
+
+    const blog = new Blog({
+      title,
+      content,
+      _user: req.user.id
+    });
+
+    try {
+      await blog.save();
+      res.send(blog);
+    } catch (err) {
+      res.send(400, err);
+    }
+  });
+};
